72ef07b5c6d34e1dcad05c8b7e1e328fd377b897
Pouyan Heyratpour <pouyan@cloudzero.ir>
2018-06-02 21:05:59 +0430

    Add USART Headers
    Signed-off-by: Pouyan Heyratpour <pouyan@cloudzero.ir>

--- src/module/usart/usart.h
+++ src/module/usart/usart.h
@@ -5,10 +5,20 @@
 #ifndef PHILUXAVR_USART_H
 #define PHILUXAVR_USART_H
 
+#ifndef F_CPU
+#  error "usart.h requires F_CPU to be defined"
+#endif
+
+#ifndef USART_BAUDRATE
+#  error "usart.h requires USART_BAUDRATE to be defined"
+#endif
+
+#define USART_BAUD_PRESCALE ((F_CPU / (USART_BAUDRATE * 16UL)) - 1)
+
 #include <io/io.h>
 
-extern io_reader* new_usart_reader();
+extern io_reader *new_usart_reader();
 
-extern io_writer* new_usart_writer();
+extern io_writer *new_usart_writer();
 
 #endif //PHILUXAVR_USART_H
